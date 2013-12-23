6a72bb2be3d9b62e00c09aaae7e7c7d457829fc0
IcmpEcho <pinnapong@gmail.com>
2013-12-23 11:22:55 +0700

    New rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HappyFriday::Application.config.secret_key_base = '2a629c67735800c4214de54e33dd9bbf9e6e8085425f9a3d59be2ba054c2ed59246eac54413f56e84e81cabf4fe5d4d6dbbd2a6d422c818a71e45c7f0309b85c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HappyFriday::Application.config.session_store :cookie_store, key: '_happy-friday_session'
