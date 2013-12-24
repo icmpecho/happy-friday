7cb7fab70bd7ec9e5dddc415ff5739148ac214c9
IcmpEcho <pinnapong@gmail.com>
2013-12-24 12:41:29 +0700

    Add next_talk method to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User
   include Mongoid::Document
+  include DateHelper
   field :name, type: String
   field :weight, type: Integer
 
@@ -10,4 +11,10 @@ class User
   def self.max_weight
   	self.all.desc(:weight).first.weight
   end
+
+  def next_talk
+  	weeks = self.weight - self.class.min_weight
+  	self.next_friday(weeks)
+  end
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,11 +1,16 @@
 require 'spec_helper'
 
 describe User do
+
+	foo = nil
+	bar = nil
+
+  before do
+		foo = FactoryGirl.create(:user, name: 'Foo', weight: 2)
+		bar = FactoryGirl.create(:user, name: 'Bar', weight: 3)
+	end
+
 	describe 'Class methods' do
-	  before do
-			foo = FactoryGirl.create(:user, name: 'Foo', weight: 2)
-			bar = FactoryGirl.create(:user, name: 'Bar', weight: 3)
-		end
 		it 'return correct min_weight' do
 			expect(User.min_weight).to eq 2
 		end
@@ -13,4 +18,15 @@ describe User do
 			expect(User.max_weight).to eq 3
 		end
 	end
+
+	describe 'Object methods' do
+		before do
+			Date.stub(:today).and_return(Date.new(2013,12,24))
+		end
+		it 'return correct next_talk' do
+			expect(foo.next_talk).to eq Date.new(2013,12,27)
+			expect(bar.next_talk).to eq Date.new(2014,1,3)
+		end
+	end
+
 end
