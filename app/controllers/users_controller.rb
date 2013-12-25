acc15e676eea1e57f35486b8cd8f5b3ce1caa938
IcmpEcho <pinnapong@gmail.com>
2013-12-25 16:17:38 +0700

    Add html rendering for users index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,10 @@
 class UsersController < ApplicationController
 	def index
 		@users = User.all.asc(:weight)
-		render json: @users, callback: params['callback']
+		respond_to do |format|
+			format.html
+			format.json { render json: @users, callback: params['callback'] }
+		end
 	end
 	def me
 		@user = session[:user_id] ? User.find(session[:user_id]) : nil
