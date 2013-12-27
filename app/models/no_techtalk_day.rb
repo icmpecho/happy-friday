fbc723dd15631fea4c4c3ebdc0b92f1d559363b5
IcmpEcho <pinnapong@gmail.com>
2013-12-27 16:47:43 +0700

    Add uniqueness validation to date

--- app/models/no_techtalk_day.rb
+++ app/models/no_techtalk_day.rb
@@ -2,5 +2,6 @@ class NoTechtalkDay
   include Mongoid::Document
   field :date, type: Date
   field :reason, type: String
-  belongs_to :reporter, class_name: 'User' 
+  belongs_to :reporter, class_name: 'User'
+  validates :date, uniqueness: true
 end
