eb8fb931c6924ae6080686924d1d39253c828b68
Pinnapong Silpsakulsuk <ping@abctech-thailand.com>
2014-01-02 11:49:19 +0700

    Fix validation errors

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,24 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
+
+  sequence :name do |n|
+    "name#{n}"
+  end
+
+  sequence :email do |n|
+    "email#{n}@abctech-thailand.com"
+  end
+
+  sequence :uid do |n|
+    "#{n}"
+  end
+
   factory :user do
-    name "MyString"
+    name
+    email
+    uid
     weight 1
   end
+
 end
